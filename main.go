22d1159ff7636e42089981a7f39a8a3127c509b1
SAYAK GHOSH <56470816+Cybersayak@users.noreply.github.com>
2024-07-26 06:49:08 +0000

    Error Handling: The function's return value (an error) is assigned to the variable err. The if statement checks whether err is not nil, which would indicate that an error occurred while attempting to start the server. If err is not nil, this line logs the error and terminates the program.log .Fatal(err) Prints the error message to the standard output (or wherever the log output is configured to go).
    Error Handling  in golang
    In this code, error handling is essential for ensuring the server starts correctly and for providing feedback if it does not. Here’s a breakdown of the error handling approach:
    
    Check for Errors: The if err := ...; err != nil construct allows the program to handle the error immediately after the operation that might fail (starting the server).
    
    Log and Terminate on Error: By using log.Fatal(err), the program ensures that if the server fails to start, the error is logged, and the program terminates, signaling that there was an issue.
    
    Example Error Scenarios
    Port Already in Use: If another process is already using the specified port, http.ListenAndServe will return an error indicating that the address is already in use.
    
    Invalid Port Number: If the portNumber variable contains an invalid value (e.g., a non-numeric string), http.ListenAndServe will return an error.
    
    Permission Denied: If the port number is below 1024 and the program does not have the necessary permissions, http.ListenAndServe will return an error.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"main/pkg/handlers"
 	"net/http"
 )
@@ -13,7 +14,14 @@ func main() {
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
-	fmt.Println(fmt.Sprintf("Starting server on port %s", portNumber))
-	_ = http.ListenAndServe(portNumber, nil)
+	/*
+		fmt.Println(fmt.Sprintf("Starting server on port %s", portNumber))
+		_ = http.ListenAndServe(portNumber, nil)
+	*/
 
+	//To Handle error properly the below code
+	fmt.Println(fmt.Sprintf("Starting server on port %s", portNumber))
+	if err := http.ListenAndServe(portNumber, nil); err != nil {
+		log.Fatal(err)
+	}
 }
